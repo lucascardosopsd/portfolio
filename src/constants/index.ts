09c782b8c827160515eb417829a2b7efbeb09335
lucas cardoso <lucascardosopsd@gmail.com>
2023-12-20 16:39:07 -0300

    data: base static site text

---
+++ src/constants/index.ts
@@ -0,0 +1,264 @@
+import { Linkedin, Mail, MapPin, Phone, PhoneIcon } from "lucide-react";
+
+export const navLinks = [
+  {
+    label: "Inicio",
+    url: "/",
+  },
+  {
+    label: "Sobre",
+    url: "/about",
+  },
+  {
+    label: "Portfólio",
+    url: "/portfolio",
+  },
+  {
+    label: "Contato",
+    url: "/contact",
+  },
+];
+export const skills = [
+  {
+    id: 1,
+    name: "Desenvolvedor Fullstack",
+    description:
+      "Desenvolvimento de aplicações web com as stacks mais modernas do mercado através de Typescript, do front-end ao back-end",
+    icon: "/icons/service-1.svg",
+  },
+  {
+    id: 2,
+    name: "UI Designer",
+    description:
+      "Design de interfaces modernas utilizando Figma, visando os aspectos mais atuais do design",
+    icon: "/icons/service-2.svg",
+  },
+  {
+    id: 3,
+    name: "User Experiênce",
+    description:
+      "Elaboração de fluxo de uso da aplicação sempre visando a melhor experiência do usuário",
+    icon: "/icons/service-3.svg",
+  },
+  {
+    id: 4,
+    name: "Responsividade",
+    description:
+      "Criação de aplicações que servem a qualquer tamanho de tela, do mobile ao desktop",
+    icon: "/icons/service-4.svg",
+  },
+];
+export const benefits = [
+  {
+    id: 1,
+    name: "Desenvolvimento customizado para atender seu negócio",
+    description:
+      "Sempre busco traduzir as necessidades e identidade da marca em um site único. Através da colaboração conjunta com o cliente, garanto que a aplicação reflita a personalidade, os valores e objetivos da marca. O resultado é uma plataforma customizada que se destaca entre os concorrentes",
+  },
+  {
+    id: 2,
+    name: "Programação criativa",
+    description:
+      "Com um olhar afiado para o estilo e muita criatividade, eu me especializo em programar sites visualmente incríveis e envolventes. Ao integrar estruturas de código elegantes, elementos interativos e princípios de design estratégicos, eu crio uma experiência de usuário imersiva e impactante. Seu site vai conquistar os visitantes, elevando a credibilidade e profissionalismo da sua marca.",
+  },
+  {
+    id: 3,
+    name: "Desenvolvimento centrado na experiência",
+    description:
+      "Dou prioridade ao desenvolvimento da experiência do usuário (UX), pensando nos seus visitantes durante todo o processo de codificação. Por meio de pesquisa extensiva e metodologias de teste com usuários, garanto que seu site seja intuitivo, fácil de navegar e guie os usuários de maneira eficaz. O resultado é uma jornada do usuário gratificante que impulsiona o envolvimento e as conversões.",
+  },
+  {
+    id: 4,
+    name: "Codigo otimizado para mobile",
+    description:
+      "No cenário atual impulsionado por dispositivos móveis, um site responsivo é essencial. Eu sou especializado em codificar designs compatíveis com dispositivos móveis que se adaptam perfeitamente a diferentes dispositivos e tamanhos de tela. Seu site manterá sua aparência e funcionalidade em computadores desktop, smartphones e tablets, permitindo que você alcance e envolva seu público de maneira eficaz.",
+  },
+  {
+    id: 5,
+    name: "Habilidade com tecnologias avançadas",
+    description:
+      "Com experiência em HTML, CSS, JavaScript e tecnologias front-end de ponta, possuo a habilidade técnica para concretizar seus conceitos de design. Desde animações fluidas até recursos interativos, eu aproveito as mais recentes tecnologias para elevar o envolvimento do usuário e proporcionar uma experiência dinâmica.",
+  },
+];
+export const portfolios = [
+  {
+    id: 1,
+    title: "Z - Aura Perfume Website",
+    link: "#",
+    images: [
+      "/images/portfolio-1.png",
+      "/images/portfolio-2.png",
+      "/images/portfolio-3.png",
+    ],
+  },
+  {
+    id: 2,
+    title: "Forhelp Charity Website",
+    link: "#",
+    images: [
+      "/images/portfolio-2.png",
+      "/images/portfolio-3.png",
+      "/images/portfolio-1.png",
+    ],
+  },
+  {
+    id: 3,
+    title: "Skill Bridge Website",
+    link: "#",
+    images: [
+      "/images/portfolio-3.png",
+      "/images/portfolio-1.png",
+      "/images/portfolio-2.png",
+    ],
+  },
+  {
+    id: 4,
+    title: "Current Vandi Website",
+    link: "#",
+    images: [
+      "/images/portfolio-4.png",
+      "/images/portfolio-5.png",
+      "/images/portfolio-6.png",
+    ],
+  },
+  {
+    id: 5,
+    title: "Fit Fusion Gym Website",
+    link: "#",
+    images: [
+      "/images/portfolio-5.png",
+      "/images/portfolio-6.png",
+      "/images/portfolio-7.png",
+    ],
+  },
+  {
+    id: 6,
+    title: "DX Digital Agency Website",
+    link: "#",
+    images: [
+      "/images/portfolio-6.png",
+      "/images/portfolio-7.png",
+      "/images/portfolio-8.png",
+    ],
+  },
+  {
+    id: 7,
+    title: "Little Learners Website",
+    link: "#",
+    images: [
+      "/images/portfolio-7.png",
+      "/images/portfolio-8.png",
+      "/images/portfolio-9.png",
+    ],
+  },
+  {
+    id: 8,
+    title: "StreamVibe Website",
+    link: "#",
+    images: [
+      "/images/portfolio-8.png",
+      "/images/portfolio-9.png",
+      "/images/portfolio-4.png",
+    ],
+  },
+  {
+    id: 9,
+    title: "YourBank Website",
+    link: "#",
+    images: [
+      "/images/portfolio-9.png",
+      "/images/portfolio-4.png",
+      "/images/portfolio-5.png",
+    ],
+  },
+];
+
+export const faqs = [
+  {
+    id: 1,
+    question: "Você pode trabalhar remotamente com clientes?",
+    answer:
+      "Com certeza! Através de canais eficazes de comunicação, como e-mail, videochamadas e ferramentas de gerenciamento de projetos, garanto uma colaboração fluida, independentemente da localização geográfica.",
+  },
+  {
+    id: 2,
+    question: "Você oferece serviços de manutenção de sites?",
+    answer:
+      "Sim, ofereço serviços de manutenção de sites desde que façam parte das tecnologias que sou familiarizado.",
+  },
+  {
+    id: 3,
+    question: "Quais tipos de site você programa?",
+    answer:
+      "Ofereço a criação de qualquer tipo de aplicação, desde um simples site para portfólio até uma plataforma de e-commerce.",
+  },
+  {
+    id: 4,
+    question: "Quais formas de pagamento são aceitas?",
+    answer:
+      "PIX e dinheiro, com o pagamento sendo repartido em 50% antes do inicio do projeto e 50% depois.",
+  },
+  {
+    id: 5,
+    question: "Como funciona o processo de criação do site?",
+    answer:
+      "Conversamos sobre suas expectativas pessoalmente ou através de vídeo chamada. A partir dai, avalio nossa conversa separadamente e estabaleço um orçamento. Após isto, nos reunimos para firmar um contrato e receber o pagamento do sinal. Então elaboramos o aspecto visual da aplicação e após aprovação do mesmo, parto para o desenvolvimento em código, onde ao final entrego um site hospedado e com domínio por 1 ano.",
+  },
+  {
+    id: 6,
+    question:
+      "E se eu quiser adicionar mais itens após o fechamento do desenvolvimento?",
+    answer:
+      "Se for o caso da adição de funcionalidades após a finalização da criação da aplicação, avalio a complexidade do peido e estabeleço um valor a parte.",
+  },
+
+  {
+    id: 7,
+    question: "Como funciona o suporte?",
+    answer:
+      "O suporte é gratuito apenas para problemas técnicos derivados do desenvolvimento e hospedagem.",
+  },
+];
+
+export const socialLinks = [
+  {
+    id: 2,
+    label: "Linkedin",
+    icon: Linkedin,
+    url: "https://www.linkedin.com/in/lcpsd/",
+  },
+  {
+    id: 3,
+    label: "Whatsapp",
+    icon: PhoneIcon,
+    url: "https://api.whatsapp.com/send?phone=5517996484654",
+  },
+];
+
+export const contactDetails = [
+  {
+    id: 1,
+    icon: Phone,
+    text: "+55(17)99648-4654",
+  },
+  {
+    id: 2,
+    icon: Mail,
+    text: "lucascardosopsd@gmail.com",
+  },
+  {
+    id: 3,
+    icon: MapPin,
+    text: "Jales-SP, Brasil",
+  },
+];
+
+export const timeline = [
+  {
+    id: 1,
+    title: "Desenvolvedor Web Pleno",
+    duration: "2021 - Present",
+    description:
+      "Atuando como freelancer através de tecnologias como React, Next, Node, Postgres, Tailwind e muito mais.",
+  },
+];
