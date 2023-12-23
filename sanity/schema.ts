8ee6448990e24a01002a054bf7bd4c4963948590
lucas cardoso <lucascardosopsd@gmail.com>
2023-12-23 14:21:02 -0300

    schema: All page titles

--- sanity/schema.ts
+++ sanity/schema.ts
@@ -1,6 +1,7 @@
 import { type SchemaTypeDefinition } from "sanity";
 import hero from "./schemas/hero";
+import titles from "./schemas/titles";
 
 export const schema: { types: SchemaTypeDefinition[] } = {
-  types: [hero],
+  types: [hero, titles],
 };
---
+++ sanity/schemas/titles.ts
@@ -0,0 +1,89 @@
+export default {
+  name: "titles",
+  type: "document",
+  title: "Títulos",
+  groups: [
+    {
+      name: "skills",
+      title: "Habilidades",
+    },
+    {
+      name: "benefits",
+      title: "Vantagens",
+    },
+    {
+      name: "portfolio",
+      title: "Portfolio",
+    },
+    {
+      name: "techs",
+      title: "Tecnologias",
+    },
+    {
+      name: "cta",
+      title: "Call to Action",
+    },
+  ],
+  fields: [
+    {
+      group: "skills",
+      name: "skillsTitle",
+      type: "string",
+      title: "Título Habilidades",
+    },
+    {
+      group: "skills",
+      name: "skillsSubTitle",
+      type: "string",
+      title: "Sub Título Habilidades",
+    },
+    {
+      group: "benefits",
+      name: "benefitsTitle",
+      type: "string",
+      title: "Título Vantagens",
+    },
+    {
+      group: "benefits",
+      name: "benefitsSubTitle",
+      type: "string",
+      title: "Sub Título Vantagens",
+    },
+    {
+      group: "portfolio",
+      name: "portfolioTitle",
+      type: "string",
+      title: "Título Portfolio",
+    },
+    {
+      group: "portfolio",
+      name: "portfolioSubTitle",
+      type: "string",
+      title: "Sub Título Portfolio",
+    },
+    {
+      group: "techs",
+      name: "techsTitle",
+      type: "string",
+      title: "Título Tecnologias",
+    },
+    {
+      group: "techs",
+      name: "techsSubTitle",
+      type: "string",
+      title: "Sub Título Tecnologias",
+    },
+    {
+      group: "cta",
+      name: "ctaTitle",
+      type: "string",
+      title: "Título CTA",
+    },
+    {
+      group: "cta",
+      name: "ctaSubTitle",
+      type: "string",
+      title: "Sub Título CTA",
+    },
+  ],
+};
