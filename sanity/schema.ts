26c7830202917d7c2da4662d2f9dcfb46fbc5391
lucas cardoso <lucascardosopsd@gmail.com>
2023-12-23 16:16:59 -0300

    schema: benefits

--- sanity/schema.ts
+++ sanity/schema.ts
@@ -2,7 +2,8 @@ import { type SchemaTypeDefinition } from "sanity";
 import hero from "./schemas/hero";
 import titles from "./schemas/titles";
 import skills from "./schemas/skills";
+import benefits from "./schemas/benefits";
 
 export const schema: { types: SchemaTypeDefinition[] } = {
-  types: [hero, titles, skills],
+  types: [hero, titles, skills, benefits],
 };
---
+++ sanity/schemas/benefits.ts
@@ -0,0 +1,17 @@
+export default {
+  name: "benefits",
+  type: "document",
+  title: "Vantagens",
+  fields: [
+    {
+      name: "title",
+      type: "string",
+      title: "Título",
+    },
+    {
+      name: "description",
+      type: "string",
+      title: "Descrição",
+    },
+  ],
+};
