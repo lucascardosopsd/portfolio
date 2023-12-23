a8e44c12bcb735692e4773d8e7c5891fc7054b19
lucas cardoso <lucascardosopsd@gmail.com>
2023-12-23 15:49:26 -0300

    schema: skills

---
+++ sanity/schemas/skills.ts
@@ -0,0 +1,24 @@
+export default {
+  name: "skills",
+  type: "document",
+  title: "Habilidades",
+  fields: [
+    {
+      name: "title",
+      type: "string",
+      title: "Título",
+    },
+
+    {
+      name: "description",
+      type: "string",
+      title: "Descrição",
+    },
+
+    {
+      name: "slIconName",
+      type: "string",
+      title: "Nome do Icone (Simple Line Icons)",
+    },
+  ],
+};
