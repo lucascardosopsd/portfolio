e1cb41cc9bd922e080bee59631a857aa4aac4866
lucas cardoso <lucascardosopsd@gmail.com>
2023-12-23 13:53:36 -0300

    schema: hero section

---
+++ sanity/schemas/hero.ts
@@ -0,0 +1,30 @@
+export default {
+  name: "hero",
+  type: "document",
+  title: "Hero Section",
+  fields: [
+    {
+      name: "greeting",
+      type: "string",
+      title: "Saudação",
+    },
+
+    {
+      name: "title",
+      type: "string",
+      title: "Título",
+    },
+
+    {
+      name: "subtitle",
+      type: "string",
+      title: "Sub Título",
+    },
+
+    {
+      name: "pic",
+      type: "image",
+      title: "Foto",
+    },
+  ],
+};
