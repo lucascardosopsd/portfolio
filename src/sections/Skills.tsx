92586e0013ed8bb067739ba5a4104806138efc1c
lucas cardoso <lucascardosopsd@gmail.com>
2023-12-24 17:19:12 -0300

    update: skills and section heading layout to dark mode

--- src/sections/Skills.tsx
+++ src/sections/Skills.tsx
@@ -24,7 +24,7 @@ const Skills = ({ titlesData, skillsData }: SKillsProps) => {
   return (
     <FadeDiv>
       <section
-        className="section-padding max-width border-t border-peach"
+        className="section-padding max-width border-t border-peach dark:border-zinc-600"
         id="skills"
         ref={ref}
       >
@@ -47,8 +47,12 @@ const Skills = ({ titlesData, skillsData }: SKillsProps) => {
                   />
                 </div>
                 <div className="text-center">
-                  <h4 className="text-grey-700">{skill.title}</h4>
-                  <p className="text-grey-600">{skill.description}</p>
+                  <h4 className="text-grey-700 dark:text-zinc-100">
+                    {skill.title}
+                  </h4>
+                  <p className="text-grey-600 dark:text-zinc-100">
+                    {skill.description}
+                  </p>
                 </div>
               </div>
             );
