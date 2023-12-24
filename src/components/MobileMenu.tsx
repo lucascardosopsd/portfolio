ed63d53ae3efdbc591d431541a064d560ff30ffc
lucas cardoso <lucascardosopsd@gmail.com>
2023-12-24 17:02:52 -0300

    update: mobile menu layout to dark

--- src/components/MobileMenu.tsx
+++ src/components/MobileMenu.tsx
@@ -23,16 +23,16 @@ const MobileMenu = () => {
       {/* Content */}
 
       <div
-        className={`w-full h-full flex flex-col bg-peach-600 fixed top-0 left-0 z-50 transition-all duration-300 ease-in-out transform ${
+        className={`w-full h-full flex flex-col bg-peach-600 dark:bg-zinc-800 fixed top-0 left-0 z-50 transition-all duration-300 ease-in-out transform ${
           isOpen ? "-translate-x-0" : "translate-x-full"
         }`}
       >
-        <div className="flex w-full items-center justify-between h-20 max-width border-b border-peach">
+        <div className="flex w-full items-center justify-between h-20 max-width border-b border-peach dark:border-zinc-600">
           <Link href="/">
             <h5 className="text-purple text-xl font-semibold">Lucas Cardoso</h5>
           </Link>
           <button
-            className="text-grey-700 hover:text-purple transition-colors"
+            className="text-grey-700 dark:text-purple hover:text-purple transition-colors"
             onClick={toggle}
           >
             <X />
@@ -60,12 +60,12 @@ const MobileMenu = () => {
           </ul>
         </nav>
 
-        <div className="w-full h-20 flex items-center justify-around max-width border-t border-peach">
+        <div className="w-full h-20 flex items-center justify-around max-width border-t border-peach dark:border-zinc-600">
           {socialLinks.map((link, index) => (
             <a
               key={index}
               href={link.url}
-              className="text-grey-700 hover:text-purple transition-colors"
+              className="text-grey-700 dark: dark:text-purple hover:text-purple transition-colors"
             >
               {link.label}
             </a>
