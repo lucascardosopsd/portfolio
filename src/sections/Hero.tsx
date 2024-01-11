4cbf099653aa7ca2d13cde406ab4eec9627f3a97
Lucas Cardoso <lucascardosopsd@gmail.com>
2024-01-11 16:53:19 -0300

    update: hero section

--- src/sections/Hero.tsx
+++ src/sections/Hero.tsx
@@ -20,12 +20,28 @@ const Hero = ({ data }: HeroProps) => {
   return (
     <FadeDiv>
       <section
-        className="flex items-center justify-center max-width flex-col-reverse tablet:flex-row space-x-4 py-4 tablet:py-0"
+        className="flex items-center justify-center max-width flex-col-reverse tablet:flex-row space-x-4 py-20 tablet:py-4 min-h-full tablet:!min-h-[calc(100svh-5rem)] relative"
         id="hero"
         ref={ref}
       >
+        {/* Absolute */}
+        <Image
+          src="/images/dashed-line.svg"
+          alt="Foto Desenvolvedor"
+          height={0}
+          width={0}
+          sizes="100vh"
+          className="h-full w-[40vw] absolute -z-20 hidden tablet:block top-0"
+        />
+
+        <span className="font-black text-8xl text-zinc-600 hidden tablet:block">
+          <p className="absolute -left-72 top-0">DESENVOLVEDOR</p>
+
+          <p className="absolute -right-40 bottom-0">FULLSTACK</p>
+        </span>
+
         {/* Left */}
-        <div className="flex-1 tablet:w:2/3 mt-10 tablet:mt-0 space-y-4">
+        <div className="flex-1 tablet:w:2/3 translate-y-10 tablet:mt-0 space-y-4">
           <div className="flex flex-col gap-3 items-center tablet:items-start max-w-2xl text-center tablet:text-left">
             <h1 className="text-zinc-100">{data.leftTitle[lang]}</h1>
             <p className="text-zinc-300">{data.leftDescription[lang]}</p>
@@ -47,7 +63,7 @@ const Hero = ({ data }: HeroProps) => {
         </div>
 
         {/* Center */}
-        <div className="flex flex-1 h-full w-full items-center justify-center py-4 tablet:py-0">
+        <div className="flex flex-1 h-full w-full items-center justify-center py-4 tablet:py-0 relative">
           <div className="relative h-[70vh] w-auto">
             <Image
               src={data.pic.asset.url}
@@ -61,9 +77,9 @@ const Hero = ({ data }: HeroProps) => {
         </div>
 
         {/* Right */}
-        <div className="flex-1 tablet:w:2/3 mt-10 tablet:mt-0 space-y-4">
+        <div className="flex-1 tablet:w:2/3 tablet:mt-0 space-y-4 -translate-y-5">
           <div className="flex flex-col gap-3 items-center tablet:items-end max-w-2xl text-center tablet:text-left">
-            <h1 className="text-zinc-100 text-center tablet:text-end">
+            <h1 className="text-zinc-100 text-center tablet:text-end  text-4xl">
               {data.rightTitle[lang]}
             </h1>
             <p className="text-zinc-300 text-center tablet:text-end">
