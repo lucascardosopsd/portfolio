c2fc87f3c004c75ae56ff3e6008f06dd1b35204e
Lucas Cardoso <lucascardosopsd@gmail.com>
2024-01-13 03:28:05 -0300

    fix: send e-mail

--- src/app/api/email/send/route.ts
+++ src/app/api/email/send/route.ts
@@ -8,13 +8,17 @@ const resend = new Resend(process.env.RESEND_API_KEY);
 export async function POST(req: Request) {
   const body = (await req.json()) as ContactFormSchemaProps;
 
+  if (!body) {
+    return new NextResponse(JSON.stringify({ ok: false }), { status: 401 });
+  }
+
   try {
     await resend.emails.send({
-      from: body?.email ? body?.email : "clientPortfolio@dev.com",
+      from: "portfolio@resend.dev",
       to: ["lucascardosopsd@gmail.com"],
       subject: `Novo Cliente: ${body.name}`,
-      text: "",
       react: ContactEmailTemplate(body),
+      text: "",
     });
 
     return new NextResponse(JSON.stringify({ ok: true }), { status: 200 });
--- src/components/ContactEmailTemplate.tsx
+++ src/components/ContactEmailTemplate.tsx
@@ -8,35 +8,35 @@ export const ContactEmailTemplate: FC<Readonly<ContactFormSchemaProps>> = ({
   message,
 }) => (
   <div className="w-full h-full flex flex-col justify-center bg-zinc-900 text-zinc-200">
-    <h4>Novo Cliente!</h4>
+    <h2>Novo Cliente!</h2>
 
-    <div className="flex flex-col items-center justify-center p-4 border border-zinc-800 rounded">
-      <h6>Nome</h6>
+    <div className="flex flex-col items-center justify-center p-4 border border-zinc-800 rounded mx-auto">
+      <h4>Nome</h4>
       <p>{name}</p>
 
       {email ? (
         <>
-          <h6>E-mail</h6>
+          <h4>E-mail</h4>
           <p>{email}</p>
         </>
       ) : (
         <>
-          <h6>Telefone</h6>
+          <h4>Telefone</h4>
           <p>{phone}</p>
         </>
       )}
 
       {email && phone && (
         <>
-          <h6>E-mail</h6>
+          <h4>E-mail</h4>
           <p>{email}</p>
 
-          <h6>Telefone</h6>
+          <h4>Telefone</h4>
           <p>{phone}</p>
         </>
       )}
 
-      <h6>Mensagem</h6>
+      <h4>Mensagem</h4>
       <p>{message}</p>
     </div>
   </div>
