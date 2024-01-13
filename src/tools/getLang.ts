c617d36b89ac902d6aaafe86e843290d18d02c91
Lucas Cardoso <lucascardosopsd@gmail.com>
2024-01-13 01:01:49 -0300

    fix: get lang

--- src/tools/getLang.ts
+++ src/tools/getLang.ts
@@ -6,7 +6,15 @@ export const getLang = () => {
   const [lang, setLang] = useState("en");
 
   useEffect(() => {
-    setLang(navigator.language.split("-")[0]);
+    const lang = navigator.language.split("-")[0];
+
+    console.log(lang);
+
+    if (lang !== "pt" || "en") {
+      setLang("en");
+    }
+
+    setLang(lang);
   }, []);
 
   return lang as "pt" | "en";
