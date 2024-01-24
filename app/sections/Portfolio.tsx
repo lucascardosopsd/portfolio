d83bb49765783b7874f74ae3c25e7680444d2093
Lucas Cardoso <lucascardosopsd@gmail.com>
2024-01-24 14:59:46 -0300

    update: portfolio projects layout

--- app/sections/Portfolio.tsx
+++ app/sections/Portfolio.tsx
@@ -1,6 +1,7 @@
 "use client";
 import FadeDiv from "@/components/FadeDiv";
 import SectionHeading from "@/components/SectionHeading";
+import { Separator } from "@/components/ui/separator";
 import { getLang } from "@/tools/getLang";
 import { watchInView } from "@/tools/watchInView";
 import { PortfolioDataProps } from "@/types/portfolio";
@@ -52,41 +53,46 @@ const Portfolio = ({ titlesData, portfolioData }: PortfolioProps) => {
           subtitle={titles.description[lang]}
         />
         <div className="flex flex-col items-center w-full">
-          <div className="grid grid-col-1 ">
+          <div className="grid grid-cols-1 tablet:grid-cols-2 gap-8">
             {portfolioData.map((project, index) => (
-              <div className="relative" key={index}>
+              <div
+                className="relative border border-zinc-600 p-4 rounded bg-zinc-900"
+                key={index}
+              >
                 <Image
                   src="/images/illustration-3.svg"
                   alt="Project"
                   height={0}
                   width={0}
                   sizes="100vh"
-                  className="h-[300px] w-auto absolute -left-16  bottom-0 top-0 my-auto hidden tablet:block"
+                  className="h-[300px] w-auto absolute -left-16  bottom-0 top-0 my-auto hidden tablet:block -z-10"
                 />
-                <div className="flex flex-col tablet:flex-row items-center tablet:gap-8 max-w-[500px] h-svh tablet:h-[80svh]">
-                  <Link
-                    href={project.link}
-                    className="relative w-full h-[400px] group flex-1 border border-transparent hover:bg-zinc-200 transition rounded"
-                    key={index}
-                  >
+                <div className="flex flex-col mb-8">
+                  <Link href={project.link} key={index}>
                     <Image
                       src={project.image.asset.url}
                       alt="Project"
                       height={0}
                       width={0}
-                      sizes="2000px"
-                      className="h-full w-full tablet:w-auto object-cover object-top tablet:object-center rounded"
+                      sizes="4000px"
+                      className="h-[300px] w-full tablet:w-auto object-cover object-top tablet:object-center rounded z-10 border border-transparent transition hover:border-white"
                     />
                   </Link>
                   <div className="flex flex-col gap-2 flex-1 justify-center">
                     <h2>{project.title[lang]}</h2>
-                    <p className="text-lg">{project.subTitle[lang]}</p>
+
+                    <p className="text-lg text-zinc-400">
+                      {project.subTitle[lang]}
+                    </p>
+
                     <p>{project.description[lang]}</p>
+
                     <span className="px-2 py-1 text-zinc-400 flex items-center justify-center text-sm border border-zinc-400 rounded w-40">
                       {project.type}
                     </span>
                   </div>
                 </div>
+                <Separator />
               </div>
             ))}
           </div>
