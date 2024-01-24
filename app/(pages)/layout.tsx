7ae0bbbe24ac46c189f261c7ae96e68e578c49a9
Lucas Cardoso <lucascardosopsd@gmail.com>
2024-01-24 15:12:05 -0300

    fix: open-graph

--- app/(pages)/layout.tsx
+++ app/(pages)/layout.tsx
@@ -18,12 +18,12 @@ export const metadata: Metadata = {
     siteName: "Lucas Cardoso",
     images: [
       {
-        url: "http://localhost:3000/images/open-graph.png",
+        url: "https://i.imgur.com/9xNWux4.png",
         width: 800,
         height: 600,
       },
       {
-        url: "http://localhost:3000/images/open-graph.png",
+        url: "https://i.imgur.com/9xNWux4.png",
         width: 1800,
         height: 1600,
         alt: "Lucas Cardoso",
