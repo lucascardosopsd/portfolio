8fd935a0a7d666a2bf2fd424334d54bdd8b97d84
Lucas Cardoso <153699076+lucascardosopsd@users.noreply.github.com>
2024-02-09 16:27:37 +0000

    config: Meta pixel page view

--- app/(pages)/(main)/layout.tsx
+++ app/(pages)/(main)/layout.tsx
@@ -1,9 +1,22 @@
 "use client";
-import { ReactNode } from "react";
+import { ReactNode, useEffect } from "react";
 import Header from "../../components/Header";
 import Footer from "../../components/Footer";
+import { usePathname, useSearchParams } from "next/navigation";
 
 const layout = ({ children }: { children: ReactNode }) => {
+  const pathname = usePathname();
+  const searchParams = useSearchParams();
+
+  useEffect(() => {
+    import("react-facebook-pixel")
+      .then((x) => x.default)
+      .then((ReactPixel) => {
+        ReactPixel.init(process.env.NEXT_PUBLIC_FB_PIXEL_ID!);
+        ReactPixel.pageView();
+      });
+  }, [pathname, searchParams]);
+
   return (
     <>
       <Header />
