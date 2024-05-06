871479e64f692a14e524a6cd7044a57c5f5346c8
Lucas Cardoso <lucascardosopsd@gmail.com>
2024-05-06 11:44:34 -0300

    update: whatsapp floating button

--- app/components/WppFloatingButton.tsx
+++ app/components/WppFloatingButton.tsx
@@ -7,18 +7,14 @@ const WppFloatingButton = () => {
   const lang = getLang();
 
   return (
-    <div className="fixed z-20 bottom-4 right-4 block tablet:hidden">
-      <Link
-        href="https://api.whatsapp.com/send?phone=5517996484654"
-        className="relative"
-      >
-        <div className="flex items-center justify-center border border-border bg-zinc-900 text-red rounded h-32 w-16 flex-col gap-2 z-10">
+    <div className="fixed z-20 bottom-4 right-4 block tablet:hidden animate-pulse">
+      <Link href="https://api.whatsapp.com/send?phone=5517996484654">
+        <div className="flex items-center justify-center border border-green-500 bg-background text-green-500 rounded h-32 w-16 flex-col gap-2 z-10 relative">
           <PiWhatsappLogoThin className="text-3xl" />
           <p className="text-xs text-center">
             {lang == "pt" ? "Fale comigo" : "Text to me"}
           </p>
         </div>
-
         <div className="h-16 w-16 bg-foreground absolute -z-10 animate-ping rounded-full inset-0 m-0" />
       </Link>
     </div>
